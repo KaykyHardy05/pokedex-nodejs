0dff978bf736e625d4e19963146eceda14419897
KaykyHardy05 <107191827+KaykyHardy05@users.noreply.github.com>
2026-08-28 19:14:36 -0300

    v8

--- main.js
+++ main.js
@@ -1,195 +1,288 @@
-body{
-    background-color: rgb(225, 225, 225);
-}
+'use strict';
 
-.Caixa_de_Alinhamento{
-    width: 100%;
-    display: flex;
-    justify-content: center;
-    
-    
-}
+
 /*
-#ajuste1{
-    padding: 25px
-}
+    pesquisar por:
+        tipos ,  nome , geraçao , região , raridade
+        ( ✔ ) , ( ✔ ),  ( ✖ )  , ( ✖ ) , ( ✖ )
+    
+    
+    exibir informação:
+        nome   , imagem (alterar entre shiny's),  tipo  , região , geração (primeira aparição) , descrição,
+        ( ✔ ) ,  ( ✔ )                        , ( ✔ )  , ( ✔ ) ,  ( ✔ )                      ,  ( ✖ )
 */
-#Botao_Filtro_off{
-    border:0 hidden;
-    background-color: rgba(0,0,0,0);
-    color: aliceblue;
-    text-align: center;
+let catapimbas;
+let pagina=document.getElementById("pageN").value;
+
+const fifoFiltroTipo=[null,null]
+const filtroTipo= {        
+    'grass'    : true,
+    'poison'   : true,
+    'fire'     : true,
+    'flying'   : true,
+    'water'    : true,
+    'bug'      : true,
+    'normal'   : true,
+    'electric' : true,
+    'ground'   : true,
+    'fighting' : true,
+    'psychic'  : true,
+    'rock'     : true,
+    'ice'      : true,
+    'ghost'    : true,
+    'dragon'   : true,
+    'fairy'    : true,
+    'steel'    : true,
+    'dark'     : true
+}
+
+
+
+
+
+function cores(x, alphaValue=1){
+    let coresPorElemento = {
+        'grass'    : `rgba(119, 204,  85, ${alphaValue})`,
+        'poison'   : `rgba(170,  85, 153, ${alphaValue})`,
+        'fire'     : `rgba(255,  68,  34, ${alphaValue})`,
+        'flying'   : `rgba(136, 153, 255, ${alphaValue})`,
+        'water'    : `rgba( 51, 153, 255, ${alphaValue})`,
+        'bug'      : `rgba(170, 187,  34, ${alphaValue})`,
+        'normal'   : `rgba(170, 170, 153, ${alphaValue})`,
+        'electric' : `rgba(255, 204,  51, ${alphaValue})`,
+        'ground'   : `rgba(221, 187,  85, ${alphaValue})`,
+        'fighting' : `rgba(185,  84,  67, ${alphaValue})`,
+        'psychic'  : `rgba(255,  85, 153, ${alphaValue})`,
+        'rock'     : `rgba(187, 170, 102, ${alphaValue})`,
+        'ice'      : `rgba(102, 204, 255, ${alphaValue})`,
+        'ghost'    : `rgba(102, 102, 187, ${alphaValue})`,
+        'dragon'   : `rgba(119, 102, 238, ${alphaValue})`,
+        'fairy'    : `rgba(238, 153, 238, ${alphaValue})`,
+        'steel'    : `rgba(170, 170, 187, ${alphaValue})`,
+        'dark'     : `rgba(119,  85,  68, ${alphaValue})`,
+        'off'      : `rgba(  35,   35,   35, 0.75)`
+    };
     
-    border-right: 3px solid rgba(0, 0, 0, 0.25);
+    return coresPorElemento[x];
 }
 
-#Botao_Filtro_on{
-    border: 0 hidden;
-    background-color:rgba(37, 37, 41, 0.25);
-    color: aliceblue;
-    text-align: center;
+
+
+
+
+const buscaApi = async() => {
+
+    console.log(`buscaApi([${fifoFiltroTipo[0]},${fifoFiltroTipo[1]}])`)
+    document.getElementById("Caixa_Pokemon").innerHTML = "";
+
+    const nomePokemon = document.getElementById('Pesquisa').value;
+    var teste;
+
+    if(nomePokemon === ""){
+        if(fifoFiltroTipo[1]!=null){
+            teste=await carregarJson(`/type/${fifoFiltroTipo[1]}`);
+        }else{
+            teste=await carregarJson(`/pokemon?limit=24&offset=${24*(pagina)}`);
+        }
+        console.log("SEM Comando");
+    }
+    else{
+        teste=await carregarJson("/pokemon/"+nomePokemon);
+        console.log("Comando pesquisa: "+nomePokemon);
+        console.log(`http://pokeapi.co/api/v2/pokemon${"/"+nomePokemon}`)
+    }
     
-    border-right: 3px solid rgba(0, 0, 0, 0.25);
-    border-bottom: 3px solid rgba(0, 0, 0, 0.25);
-    border-end-end-radius: 10px;
     
 
-}
-
-.cabecalho_Filtros{
-    grid-column: 1 / 2;
-    grid-row: 1 / 2;
-}
-.cabecalho_FiltroFill{
-    grid-column: 2 / 3;
-    grid-row: 1 / 2;
-}
-
-.cabecalho_FiltroFill2{
-    grid-column: 1 / -1;
-    grid-row: 2 / 3;
-    background-color:black;
-}
-
-
-.cabecalho_Filtro2{
-    width:400px;
-    height:24px;
-    grid-column: 3 / -2;
-    grid-row: 1 / 2;
-}
-
-#Caixa_Filtro{
-    width: 580px;
-    height: 25px;
-    
-    background-color:rgba(37, 37, 41, 0.25);
-    
-    margin: 12px 0 0 0;
-    
-    border: 3px solid rgba(0, 0, 0, 0.45);
-    border-radius: 10px;
-
-    color: azure;
-
-
-    display: grid;
-    grid-template-columns: repeat(6, 1fr);
-    grid-template-rows: repeat(15, 1fr);
-    row-gap: 2px;
-
-
-}
-
-#Caixa_Pesquisa1{
-    width: 500px;
-    height: 50px;
-    border: 2px double  rgb(255, 255, 255);
-    border-radius: 50px;
-    padding: 2px;
-    
-}
-
-#Caixa_Pesquisa2{
-    width: auto;
-    height: 40px;
-    background-color: rgb(255, 255, 255);
-    border: 5px double  rgb(255, 255, 255);
-    border-radius: 50px;
-    
-    padding: 0 10px 0 10px;
-    text-align: left;
-    display: flex;
-    align-items: center;
-
-}
-
-#Pesquisa{
-    border: 0 solid;
-    border-radius: 5px;
-    width: 400px;
-}
-
-
-
-#Caixa_Pokemon {
-    width: 80%;
-    min-height: 100px;
-
-    border: 5px solid rgb(255, 255, 255);
-    border-radius: 50px;
-
-    padding: 20px;
-    box-sizing: border-box;
-
-    display: flex;
-    flex-direction: row;
-    flex-wrap: wrap;
-    gap: 20px;
-
-    justify-content: center;
-}
-
-.pokeballInner{
-    background: radial-gradient(rgba(255, 255, 255, 1  )20%,
-                                rgba(  0,   0,   0, 1  )20%,
-                                rgba(  0,   0,   0, 0  )25%,
-                                rgba(  0,   0,   0, 0  )55%,
-                                rgba(  0,   0,   0, 0.5)71%,
-                                rgba(  0,   0,   0, 0  )71%);
-    width:150px;
-    height:150px;
-
-    display: flex;
-    justify-content: center;
-    align-items: center;
-
-}
-
-.paginaSelector{
-    width: 25px;
-    text-align: center;
-
-    background-color: rgba(0,0,0,0);
-    border: 0px; /*limpa qualquer borda que tinha antes*/
-  
-    border-top: 2px solid black;
-    border-bottom: 2px  solid black;
-}
-
-.container {
-    width: 150px;
-    height: 150px;
-
-    text-align: center;
-    border: 2px solid black;
-    border-radius: 100%;
-    
-    flex-shrink: 0;
-}
-
-.filtro_Elemento{
-    width: 75px;
-    height: 25px;
-
-    border: 3px solid rgba(35,35,35,0.5);
-    /*border-radius: 15px;*/
-    text-align: center;
+    if(teste.results){
+        for (const pokemonResults of teste.results) {
+            const pokemon= await carregarJson("/pokemon/"+pokemonResults.name);
+            
+            document.getElementById("Caixa_Pokemon").innerHTML += criarContainer(infoPokemons(pokemon)); 
+        }
+    }
+    if(teste.pokemon){
+        for(let i=24*pagina,x=0; i<teste.pokemon.length;i++){
+            if(x>=24){continue}
+            const pokemon= await carregarJson(`/pokemon/${teste.pokemon[i].pokemon.name}`);
+            
+            if(fifoFiltroTipo[0]!=null){
+                if(infoPokemons(pokemon).tipagem.tipo1 == teste.name){
+                    if(!(infoPokemons(pokemon).tipagem.tipo2 == fifoFiltroTipo[0] || infoPokemons(pokemon).tipagem.tipo2 == fifoFiltroTipo[1])){continue;}
+                }
+                if(infoPokemons(pokemon).tipagem.tipo2 == teste.name){
+                    if(!(infoPokemons(pokemon).tipagem.tipo1 == fifoFiltroTipo[0] || infoPokemons(pokemon).tipagem.tipo1 == fifoFiltroTipo[1])){continue;}
+                }
+            }
+            
+            document.getElementById("Caixa_Pokemon").innerHTML += criarContainer(infoPokemons(pokemon));
+            x++;
+        }
+    }    
+    else{
+        document.getElementById("Caixa_Pokemon").innerHTML += criarContainer(infoPokemons(teste));
+    }
 
 }
 
 
 
-.W_space1{
-    padding-bottom: 70px;
-}
-.W_space2{
-    padding-bottom: 20px;
+
+
+let infoPokemons = function(Pokemon){
+    return {
+            ["nome"]:Pokemon.name,
+            ["imagem"]:{"normal":Pokemon.sprites.other["official-artwork"]["front_default"],"shiny":Pokemon.sprites.other["official-artwork"]["front_shiny"]},
+            ["tipagem"]:{"tipo1":Pokemon.types[0].type['name'],"tipo2":Pokemon.types[1]?.type['name']},
+            ["regiao"]:Pokemon.encounters,
+            ["geracao"]:Pokemon.game_indices[0].version["name"],
+        };
+};
+
+
+
+
+
+async function carregarJson(Pokemon){
+    const url = `http://pokeapi.co/api/v2${Pokemon}`;
+
+    const dados = await fetch(url);
+    const jason = await dados.json();
+    return jason;
 }
 
 
-input::-webkit-outer-spin-button,
-input::-webkit-inner-spin-button {
-  -webkit-appearance: none;
-  margin: 0;
+
+
+
+function criarContainer(info){console.log("Container Criado!")
+    return `
+        <div class="container" style="background: linear-gradient(145deg, ${cores(info.tipagem.tipo1)}47%, rgba(0, 0, 0, 1)47%,rgba(0, 0, 0, 1)53%,   ${cores(info.tipagem.tipo2?info.tipagem.tipo2:info.tipagem.tipo1, .65)}53%)">
+        <div class="pokeballInner">
+                <img src="${info.imagem.normal}" alt="${info.nome}" style="width:120px;height:120px;">
+                
+            </div>
+            <p>${info.nome}</p>
+        </div>
+    `;
 }
+
+
+
+
+
+
+function retroceder(){
+    if(pagina > 0){
+        pagina--;
+    }else{
+        pagina= Math.floor(1025/24);    
+    }
+    document.getElementById("pageN").value = pagina;
+    buscaApi();
+}
+
+function paginaIdentifier(){
+    const seletor = document.getElementById("pageN").value;
+    pagina = seletor;
+    buscaApi();
+}
+
+function avancar(){    
+    if(pagina >= Math.floor(1025/24)){
+        pagina =0;
+    }else{pagina++;}
+    document.getElementById("pageN").value = pagina;
+    buscaApi();
+}
+
+
+
+
+
+
+async function filtros(){
+    const listaTipos= await carregarJson("/type")
+    const div=document.getElementById("Caixa_Filtro");
+
+    if(document.getElementById("Botao_Filtro_off")){
+        document.getElementById("Botao_Filtro_off").id= "Botao_Filtro_on";
+
+        div.style.width="500px";
+        div.style.height="350px";
+
+        for (const tipos of listaTipos.results) {
+            if(tipos.name == "stellar" || tipos.name == "unknown"){continue}
+            div.innerHTML+=`
+                    <button class="filtro_Elemento" id="filtro_${tipos.name}" onclick="inverterValorElemento('${tipos.name}')" style="background-color:${cores(filtroTipo[tipos.name]?tipos.name:'off')}">
+                        ${tipos.name}
+                    </button>
+                `;
+    }
+    }else if(document.getElementById("Botao_Filtro_on")){
+        document.getElementById("Botao_Filtro_on").id= "Botao_Filtro_off";
+
+        div.style.width="530px";
+        div.style.height="25px";
+        for(let tipos in filtroTipo){
+            document.getElementById(`filtro_${tipos}`).remove();
+        }
+    }
+}
+
+
+
+
+
+function inverterValorElemento(x){
+    // Verifica (as 2 casas) para ver se o elemento 'x' ja esta selecionado.
+    // {TRUE} :> remove tal elemento 'x', e adiciona null a casa 0
+    if(fifoFiltroTipo[0] == x ){
+        fifoFiltroTipo.splice(0,1,null);
+    }
+    else if(fifoFiltroTipo[1] == x ){
+        fifoFiltroTipo.splice(1,1,null);
+    }else{
+        // Remove o primeiro elemento
+        fifoFiltroTipo.splice(0,1);
+        
+        // Substitui o segundo elemento se 'filtroTipo' do elemento 'x' for TRUE por 'x'
+        fifoFiltroTipo.splice(1,1,x);
+        //console.log(`pos adicionar: {${fifoFiltroTipo[0]}, ${fifoFiltroTipo[1]}}`)
+        
+        // Os elementos que estiverem dentro de 'fifoFiltroTipo' tem o valor TRUE, ao contrario sera FALSE
+        for(var i in filtroTipo){
+            if(i == fifoFiltroTipo[0] || i == fifoFiltroTipo[1]){
+                console.log(`filtroTipo[${i}] = true;`)
+                filtroTipo[i]= true;
+
+            }else{
+                filtroTipo[i]= false;
+            }
+            
+        }
+    }
+
+    for(let i=0,x=0;i<filtroTipo.length;i++){
+        if(!fifoFiltroTipo[i]){x++}
+        if(x=filtroTipo.length){for(let valores of fifoFiltroTipo){!valores}}
+    }
+    console.log(fifoFiltroTipo)
+    atualizarAparenciaFiltroTipos()
+
+    buscaApi([fifoFiltroTipo[0],fifoFiltroTipo[1]]);
+}
+
+
+
+
+
+
+function atualizarAparenciaFiltroTipos(){
+    for(var i in filtroTipo){
+        document.getElementById(`filtro_${i}`).style["background-color"] = cores(filtroTipo[i]?i:'off');
+        document.getElementById(`filtro_${i}`).style.color = filtroTipo[i]?"black":"white";
+    }
+}
+
+buscaApi();
